9e0174a5927da8bbf1911a70f443f305d8b58357
Alex Chang <75132115+alexch1234@users.noreply.github.com>
2023-12-09 19:06:26 +0000

    feat: Add Etch-a-Sketch functionality

--- scripts.js
+++ scripts.js
@@ -1,12 +1,19 @@
 const container = document.querySelector('.container');
 
-for (let i = 0; i < 4; i++) {
+for (let i = 0; i < 16; i++) {
     const gridRow = document.createElement('div');
     gridRow.classList.toggle("grid-row");
-    for (let item = 0; item < 4; item++) {
+    for (let item = 0; item < 16; item++) {
         const gridItem = document.createElement('div');
         gridItem.classList.toggle('grid-item');
         gridRow.appendChild(gridItem);
     }
     container.appendChild(gridRow);
 }
+
+const gridBoxes = document.querySelectorAll('.grid-item');
+gridBoxes.forEach(box => {
+    box.addEventListener('pointerenter', (e) => {
+        e.target.style = 'background-color: brown';
+    })
+});
